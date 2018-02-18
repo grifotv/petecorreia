a77fbadedda9c9f016f6e9b9dbfc80c323e6eab7
Pete Correia <pete@petecorreia.com>
2018-02-18 14:49:56 +0000

    Add eslint

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import styled from 'styled-components';
 import VisuallyHidden from '../VisuallyHidden';
 
+const Wrapper = styled.header`
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	z-index: 2;
+`;
+
 const Header = () => (
-	<header>
+	<Wrapper>
 		<VisuallyHidden>Logo</VisuallyHidden>
-	</header>
+	</Wrapper>
 );
 
 export default Header;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -17,7 +17,6 @@ const TemplateWrapper = ({ children }) => (
 				},
 			]}
 		/>
-		<Header />
 		{children()}
 	</div>
 );
